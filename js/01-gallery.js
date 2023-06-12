e782471047f11bfc202595eaff24002619673d8d
erik465 <ehalasz06@gmail.com>
2023-06-12 19:30:21 +0200

    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery')
+
+galleryItems.forEach(galleryItem => {
+    galleryEl.insertAdjacentHTML("afterbegin", `<li class="gallery__item">
+    <a class="gallery__link" href="${galleryItem.original}">
+      <img
+        class="gallery__image"
+        src="${galleryItem.preview}"
+        data-source="${galleryItem.original}"
+        alt="${galleryItem.description}"
+      />
+    </a>
+  </li>`)
+})
+
+galleryEl.addEventListener("click", (e) =>{
+    e.preventDefault()
+    if(e.target.nodeName !== "IMG"){
+        return
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${e.target.src}" width="800" height="600">
+`)
+
+    instance.show()
+    
+})
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery')
+
+
+
+galleryItems.forEach(galleryItem => {
+    galleryEl.insertAdjacentHTML("afterbegin", `<li class="gallery__item">
+    <a class="gallery__link" href="${galleryItem.original}">
+       <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+    </a>
+ </li>`)
+})
+
+galleryEl.addEventListener("click", (e) =>{
+    if(e.target.nodeName == "IMG"){
+        e.preventDefault()
+        const lightbox = new SimpleLightbox('.gallery a', {
+            captions : true,
+            captionSelector  : 'self',
+            captionType: 'attr',
+            captionsData : e.target.alt, 
+            captionPosition : 'bottom',
+            captionDelay : 0,
+            captionClass : "caption"
+        });
+    }
+
+    
+})
